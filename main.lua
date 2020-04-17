89960283eb5a68795dcbb45cd37328eecef3b0f8
civilianzero <civilianzero@icloud.com>
2020-04-17 18:05:09 -0400

    dummy out some main.lua basics, include table.show for managing save data

--- main.lua
+++ main.lua
@@ -1,8 +1,34 @@
 function love.load()
+	-- required files
+	require('show')
+	-- required libraries
+	Anim8 = require('libraries.anim8-master.anim8')
+	CameraFile = require('libraries.hump-master.camera')
+	Tiled = require('libraries.Simple-Tiled-Implementation-master.sti')
+
+	-- table for including sprites
+	Sprites = {}
+
+	-- table for storing save data
+	SaveData = {}
+
+	-- check for saved data, load using table.show if exists
+	if love.data.getInfo("data.lua") then
+		local data = love.filesystem.load("data.lua")
+		data()
+	end
+
+	-- TiledMap = Tiled('assets/maps/TiledMap.lua')
+
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+end
+
+-- utility function for determing distance between two points
+function DistanceBetween(x1, y1, x2, y2)
+	return math.sqrt((y2 - y1)^2 + (x2 - x1)^2)
 end

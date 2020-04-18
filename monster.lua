fa76490d836f95b3cce66da6fe31293af430f14f
civilianzero <civilianzero@icloud.com>
2020-04-18 15:52:43 -0400

    gave monsters a blood type, fixed incorrect implmentation of obj generation

--- monster.lua
+++ monster.lua
@@ -1,12 +1,13 @@
 Monsters = {}
 
-function SpawnMonster(x, y)
+function SpawnMonster()
 	for i,obj in pairs(OverWorld.layers["monsters"].objects) do
 		local monster = {}
-		monster.body = love.physics.newBody(World, x, y, "dynamic")
-		monster.shape = love.physics.newRectangleShape(6, 11, 12, 12)
+		monster.body = love.physics.newBody(World, obj.x, obj.y, "dynamic")
+		monster.shape = love.physics.newRectangleShape(obj.width/2, obj.height/2, obj.width, obj.height)
 		monster.fixture = love.physics.newFixture(monster.body, monster.shape)
 		monster.dead = false
+		monster.bloodType = math.floor(math.random(1, 4))
 
 		monster.grid = Anim8.newGrid(16, 16, 32, 32)
 		monster.animation = Anim8.newAnimation(monster.grid('1-2',1), 0.5)

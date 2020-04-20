fa96e54592e45ed1259dd63f11cc8fcb91962762
civilianzero <civilianzero@icloud.com>
2020-04-19 22:11:13 -0400

    adjusted player collision

--- entities/player.lua
+++ entities/player.lua
@@ -5,7 +5,7 @@ function Player:init(map)
 		Player.x = spawn.x
 		Player.y = spawn.y
 	end
-	World:add(Player, Player.x, Player.y, 10, 14)
+	World:add(Player, Player.x, Player.y, 12, 12)
 	Player.goalX = Player.x
 	Player.goalY = Player.y
 	Player.speed = 30
